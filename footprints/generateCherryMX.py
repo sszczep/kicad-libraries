afbe96d27f3a6006f16f669b33c3945bc23fee72
Sebastian Szczepański <s.szczepanski00@gmail.com>
2020-11-07 20:08:37 +0100

    Improve Cherry MX footprint generation, change naming convention

--- footprints/generateCherryMX.py
+++ footprints/generateCherryMX.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 
 sizesToGenerate=["1.00", "1.25", "1.50", "1.75", "2.00", "2.25", "2.50", "2.75", "3.00", "4.00", "4.50", "5.50", "6.00", "6.25", "6.50", "7.00", "8.00", "9.00", "9.75", "10.00"]
 
@@ -90,6 +91,23 @@ componentsList = {
     (rotate (xyz 0 0 0))
   )""",
 
+  "LTST-A683CEGBW-HS": """
+  (fp_line (start -1.7 3.25) (end -2 3.25) (layer B.SilkS) (width 0.12))
+  (fp_line (start -2 3.25) (end -2 3.55) (layer B.SilkS) (width 0.12))
+  (fp_line (start -1.7 3.55) (end 1.7 3.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start 1.7 3.55) (end 1.7 6.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start 1.7 6.55) (end -1.7 6.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start -1.7 6.55) (end -1.7 3.55) (layer Edge.Cuts) (width 0.05))
+  (pad 3 smd rect (at -2.95 4.1) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 4 smd rect (at -2.95 6) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 5 smd rect (at 2.95 4.1) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 6 smd rect (at 2.95 6) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (model "${{KIPRJMOD}}/models/LTST-A683CEGBW.step"
+    (offset (xyz 0 -5.05 -1.87))
+    (scale (xyz 1 1 1))
+    (rotate (xyz 0 0 0))
+  )""",
+
   "LTST-A683CEGBW-Rotated": """
   (fp_line (start 1.7 6.85) (end 2 6.85) (layer B.SilkS) (width 0.12))
   (fp_line (start 2 6.85) (end 2 6.55) (layer B.SilkS) (width 0.12))
@@ -97,10 +115,27 @@ componentsList = {
   (fp_line (start 1.7 3.55) (end 1.7 6.55) (layer Edge.Cuts) (width 0.05))
   (fp_line (start 1.7 6.55) (end -1.7 6.55) (layer Edge.Cuts) (width 0.05))
   (fp_line (start -1.7 6.55) (end -1.7 3.55) (layer Edge.Cuts) (width 0.05))
-  (pad 3 smd rect (at 2.6 5.8 180) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
-  (pad 4 smd rect (at 2.6 4.3 180) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
-  (pad 5 smd rect (at -2.6 5.8 180) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
-  (pad 6 smd rect (at -2.6 4.3 180) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
+  (pad 6 smd rect (at -2.6 4.3) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
+  (pad 5 smd rect (at -2.6 5.8) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
+  (pad 4 smd rect (at 2.6 4.3) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
+  (pad 3 smd rect (at 2.6 5.8) (size 1.8 0.9) (layers B.Cu B.Paste B.Mask))
+  (model "${{KIPRJMOD}}/models/LTST-A683CEGBW.step"
+    (offset (xyz 0 -5.05 -1.87))
+    (scale (xyz 1 1 1))
+    (rotate (xyz 0 0 180))
+  )""",
+
+  "LTST-A683CEGBW-Rotated-HS": """
+  (fp_line (start 1.7 6.85) (end 2 6.85) (layer B.SilkS) (width 0.12))
+  (fp_line (start 2 6.85) (end 2 6.55) (layer B.SilkS) (width 0.12))
+  (fp_line (start -1.7 3.55) (end 1.7 3.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start 1.7 3.55) (end 1.7 6.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start 1.7 6.55) (end -1.7 6.55) (layer Edge.Cuts) (width 0.05))
+  (fp_line (start -1.7 6.55) (end -1.7 3.55) (layer Edge.Cuts) (width 0.05))
+  (pad 6 smd rect (at -2.95 4.1) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 5 smd rect (at -2.95 6) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 4 smd rect (at 2.95 4.1) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
+  (pad 3 smd rect (at 2.95 6) (size 2.5 1.1) (layers B.Cu B.Paste B.Mask))
   (model "${{KIPRJMOD}}/models/LTST-A683CEGBW.step"
     (offset (xyz 0 -5.05 -1.87))
     (scale (xyz 1 1 1))
@@ -123,105 +158,80 @@ componentsList = {
 )"""
 }
 
-# Possible variants:
-# 1: Plate/PCB mounted
-# 2: Stabilizer Plate/PCB mounted
-# 3: no LED/LED/LTST-A683CEGBW
-# 4: Normal switch mount/Kail Socket for hot-swap compatibility
-
-variantsList = [
-  ("Plate", ["BaseStart", "Pins", "BaseEnd"]),
-  ("Plate_StabWireTop", ["BaseStart", "Pins", "StabWireTop", "BaseEnd"]),
-  ("Plate_StabWireBottom", ["BaseStart", "Pins", "StabWireBottom", "BaseEnd"]),
-  ("Plate_LED", ["BaseStart", "Pins", "LED", "BaseEnd"]),
-  ("Plate_StabWireTop_LED", ["BaseStart", "Pins", "StabWireTop", "LED", "BaseEnd"]),
-  ("Plate_StabWireBottom_LED", ["BaseStart", "Pins", "StabWireBottom", "LED", "BaseEnd"]),
-  ("Plate_LTST-A683CEGBW", ["BaseStart", "Pins", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("Plate_StabWireTop_LTST-A683CEGBW", ["BaseStart", "Pins", "StabWireTop", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_StabWireBottom_LTST-A683CEGBW", ["BaseStart", "Pins", "StabWireBottom", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_StabWireTop_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "StabWireTop", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("Plate_StabWireBottom_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "StabWireBottom", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("Plate_KailhSocket", ["BaseStart", "KailhSocket", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireTop", ["BaseStart", "KailhSocket", "StabWireTop", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireBottom", ["BaseStart", "KailhSocket", "StabWireBottom", "BaseEnd"]),
-  ("Plate_KailhSocket_LED", ["BaseStart", "KailhSocket", "LED", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireTop_LED", ["BaseStart", "KailhSocket", "StabWireTop", "LED", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireBottom_LED", ["BaseStart", "KailhSocket", "StabWireBottom", "LED", "BaseEnd"]),
-  ("Plate_KailhSocket_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_KailhSocket_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireTop_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "StabWireTop", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireBottom_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "StabWireBottom", "LTST-A683CEGBW", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireTop_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "StabWireTop", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("Plate_KailhSocket_StabWireBottom_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "StabWireBottom", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB", ["BaseStart", "Pins", "PCB", "BaseEnd"]),
-  ("PCB_StabWireTop", ["BaseStart", "Pins", "PCB", "StabWireTop", "BaseEnd"]),
-  ("PCB_StabWireBottom", ["BaseStart", "Pins", "PCB", "StabWireBottom", "BaseEnd"]),
-  ("PCB_LED", ["BaseStart", "Pins", "PCB", "LED", "BaseEnd"]),
-  ("PCB_StabWireTop_LED", ["BaseStart", "Pins", "PCB", "StabWireTop", "LED", "BaseEnd"]),
-  ("PCB_StabWireBottom_LED", ["BaseStart", "Pins", "PCB", "StabWireBottom", "LED", "BaseEnd"]),
-  ("PCB_LTST-A683CEGBW", ["BaseStart", "Pins", "PCB", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "PCB", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB_StabWireTop_LTST-A683CEGBW", ["BaseStart", "Pins", "PCB", "StabWireTop", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_StabWireBottom_LTST-A683CEGBW", ["BaseStart", "Pins", "PCB", "StabWireBottom", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_StabWireTop_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "PCB", "StabWireTop", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB_StabWireBottom_LTST-A683CEGBW-Rotated", ["BaseStart", "Pins", "PCB", "StabWireBottom", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB_KailhSocket", ["BaseStart", "KailhSocket", "PCB", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireTop", ["BaseStart", "KailhSocket", "PCB", "StabWireTop", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireBottom", ["BaseStart", "KailhSocket", "PCB", "StabWireBottom", "BaseEnd"]),
-  ("PCB_KailhSocket_LED", ["BaseStart", "KailhSocket", "PCB", "LED", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireTop_LED", ["BaseStart", "KailhSocket", "PCB", "StabWireTop", "LED", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireBottom_LED", ["BaseStart", "KailhSocket", "PCB", "StabWireBottom", "LED", "BaseEnd"]),
-  ("PCB_KailhSocket_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "PCB", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_KailhSocket_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "PCB", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireTop_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "PCB", "StabWireTop", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireBottom_LTST-A683CEGBW", ["BaseStart", "KailhSocket", "PCB", "StabWireBottom", "LTST-A683CEGBW", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireTop_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "PCB", "StabWireTop", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
-  ("PCB_KailhSocket_StabWireBottom_LTST-A683CEGBW-Rotated", ["BaseStart", "KailhSocket", "PCB", "StabWireBottom", "LTST-A683CEGBW-Rotated", "BaseEnd"]),
+variants = [
+  [None, "PCB"],
+  ["Pins", "KailhSocket"],
+  [None, "StabWireTop", "StabWireBottom"],
+  [None, "LED", "LTST-A683CEGBW", "LTST-A683CEGBW-Rotated", "LTST-A683CEGBW-HS", "LTST-A683CEGBW-Rotated-HS"],
 ]
 
-def generateFootprint(variant, size):
-  (name, components) = variant
-  
-  name = f"CherryMX_{size}u_{name}"
+def generateFootprints():
+  for variant in itertools.product(*variants):
+    # Filter None values
+    components = [component for component in variant if component is not None]
 
-  mountType = "PCB" if "PCB" in components else "Plate"
-  usingKailhSocket = "yes" if "KailhSocket" in components else "no"
-  stabilizer = ("n/a" if float(size) < 2 else ("PCB mounted (Wire Top)" if "StabWireTop" in components else ("PCB mounted (Wire Bottom)" if "StabWireBottom" in components else "Plate mounted")))
-  lighting = ("LTST-A683CEGBW" if "LTST-A683CEGBW" in components else ("LTST-A683CEGBW (rotated)" if "LTST-A683CEGBW-Rotated" in components else ("2 pin LED" if "LED" in components else "none")))
+    # Create directory containing all sizes of given variant
+    dirname = f"CherryMX_{'_'.join(components)}.pretty".replace("_Pins", "")
+    os.makedirs(dirname, exist_ok = True)
 
-  description = "Cherry MX switch footprint. "
-  description += f"Size: {size}u"
-  description += f", Mount type: {mountType}"
-  description += f", Using Kailh Socket: {usingKailhSocket}"
-  description += f", Stabilizer: {stabilizer}"
-  description += f", Lighting: {lighting}"
+    for size in sizesToGenerate:
+      if size not in stabSpacings and ("StabWireTop" in components or "StabWireBottom" in components):
+        continue
 
-  keywords = name.replace("_", " ")
+      name = f"CherryMX_{size}u_{'_'.join(components)}".replace("_Pins", "")
 
-  code = ""
-  for component in components:
-    code += componentsList[component].format(
-      name=name, 
-      description=description, 
-      keywords=keywords, 
-      outlineSize=float(size) * unit / 2, 
-      stabSpacing=stabSpacings[size] / 2 if size in stabSpacings else "",
-    )
+      # Generate description
+      mountType = "PCB" if "PCB" in components else "Plate"
 
-  return (name, code)
+      usingKailhSocket = "yes" if "KailhSocket" in components else "no"
 
-for variant in variantsList:
-  dirname = f"CherryMX_{variant[0]}.pretty"
-  os.makedirs(dirname, exist_ok=True)
+      stabilizer = (
+        "n/a" if float(size) < 2 else (
+          "PCB mounted (Wire Top)" if "StabWireTop" in components else (
+            "PCB mounted (Wire Bottom)" if "StabWireBottom" in components else "Plate mounted"
+          )
+        )
+      )
+      
+      lighting = "none"
+      lightingMap = {
+        "LTST-A683CEGBW": "LTST-A683CEGBW",
+        "LTST-A683CEGBW-Rotated": "LTST-A683CEGBW (rotated)",
+        "LTST-A683CEGBW-HS": "LTST-A683CEGBW for hand-soldering",
+        "LTST-A683CEGBW-Rotated-HS": "LTST-A683CEGBW (rotated) for hand-soldering",
+        "LED": "2 pin LED"
+      }
+      
+      for component in components:
+        if component in lightingMap:
+          lighting = lightingMap[component]
+          break
 
-  for size in sizesToGenerate:
-    if ("StabWireTop" in variant[1] or "StabWireBottom" in variant[1]) and size not in stabSpacings:
-      continue
+      description = "Cherry MX switch footprint. "
+      description += f"Size: {size}u"
+      description += f", Mount type: {mountType}"
+      description += f", Using Kailh Socket: {usingKailhSocket}"
+      description += f", Stabilizer: {stabilizer}"
+      description += f", Lighting: {lighting}"
 
-    (name, code) = generateFootprint(variant, size)
-    file = open(f"{dirname}/{name}.kicad_mod", "w+")
-    file.writelines(code)
-    file.close()
+      keywords = name.replace("_", " ")
 
-    print(f"Generated: {name}.kicad_mod")
+      # Generate output code
+      code = ""
+      for component in ["BaseStart", *components, "BaseEnd"]:
+        code += componentsList[component].format(
+          name=name, 
+          description=description, 
+          keywords=keywords, 
+          outlineSize=float(size) * unit / 2, 
+          stabSpacing=stabSpacings[size] / 2 if size in stabSpacings else "",
+        )
+
+      # Save footprint to file
+      file = open(f"{dirname}/{name}.kicad_mod", "w+")
+      file.writelines(code)
+      file.close()
+
+      print(f"Generated: {name}.kicad_mod")
+
+generateFootprints()
